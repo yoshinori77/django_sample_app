9f4c16328a95fab49db3e780b6be46fd21ea69df
yoshinori77 <yoshinoriyamashita123@gmail.com>
2018-10-16 23:43:22 +0900

    WhiteNoise v4.0に対応。wsgi.pyに記載する必要がなくなった

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    # 'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
