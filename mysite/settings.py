525a46fa49de80d700286eb13a1fd63b14c9b574
yoshinori77 <yoshinoriyamashita123@gmail.com>
2018-10-17 10:42:57 +0900

    ビューを追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    # 'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
